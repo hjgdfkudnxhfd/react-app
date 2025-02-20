9bff2e7d3fd46d1258c4e424949ee2bf90dc4025
hjgdfkudnxhfd <valeev@rtural.ru>
2025-02-20 14:51:49 +0500

    fix search

--- src/components/TodoItems/components/SearchInput.jsx
+++ src/components/TodoItems/components/SearchInput.jsx
@@ -5,11 +5,10 @@ import {styled} from 'styled-components';
 const Input = styled.input``;
 
 // Разобраться, почему не срабатывает ввод символов в строку
-export const SearchInput = ({value, setValue, setSortedItems}) => {
+export const SearchInput = ({value, setValue}) => {
   const onInputChange = (event) => {
     if (setValue) { 
       setValue(event.nativeEvent.target.value);
-      setSortedItems(null);
     }
   }
 
